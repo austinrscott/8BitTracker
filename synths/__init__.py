d693052a25990549dd141aac56485392b635bdc5
Samuel Volk <samivolk16@hotmail.com>
2014-11-06 23:34:38 -0500

    Added support for float durations

--- synths/__init__.py
+++ synths/__init__.py
@@ -26,23 +26,23 @@ def make_beep(frequency, duration):
     '''
 
     # Creates the waveform's period, measured by the number of sound samples it spans across.
-    wave_period = SOUND_SAMPLERATE // frequency
+    wave_period = SOUND_SAMPLERATE / frequency
 
     # Creates a numpy array representing one of those waves.
     wave = np.zeros(wave_period, np.int16)
-    wave[0:wave_period//2] = -32767
-    wave[wave_period//2:-1] = 32767
+    wave[0:wave_period/2] = -32767
+    wave[wave_period/2:-1] = 32767
 
     # Multiplies the array until it reaches the desired duration.
-    total_sample_length = (duration * SOUND_SAMPLERATE) // 1
-    wave_list = [wave for i in range((total_sample_length // wave_period))]
+    total_sample_length = (duration * SOUND_SAMPLERATE)
+    wave_list = [wave for i in range(int(total_sample_length / wave_period))]
     final_wave = np.concatenate(wave_list)
 
     # Returns the pygame.mixer.Sound object.
     return pygame.sndarray.make_sound(final_wave)
 
-# Testing beep. This one lasts for one second and is at 880Hz (A5).
-sound = make_beep(880, 1)
+# Testing beep. This one lasts for 100 milliseconds and is at 880Hz (A5).
+sound = make_beep(880, .1)
 
 # Initialize the pygame display so that the program can receive keyboard input (requires window focus)
 pygame.display.set_mode((640, 480))
