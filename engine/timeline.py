ab43098e963be97993bec12ec58eea75b5ff46e3
Samuel Volk <samivolk16@hotmail.com>
2014-12-14 22:55:20 -0500

    Added song object

--- engine/timeline.py
+++ engine/timeline.py
@@ -1,4 +1,14 @@
 '''
 timeline.py will manage the project timeline, which holds all data about the current song.
 '''
-__author__ = 'Austin Scott'
+__author__ = 'Austin Scott and Samuel Volk'
+
+
+class song():
+    def __init__(self):
+        self.length = 0
+        self.data = [[], [], []]    # three channels
+
+    def clear(self):
+        self.length = 0
+        self.data = []
