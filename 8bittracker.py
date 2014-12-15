dd6120dace999897e2b50d27bfbb442db462397a
Samuel Volk <samivolk16@hotmail.com>
2014-12-14 20:11:01 -0500

    Moved main loop code from gui/__init__.py to 8bittracker.py

--- 8bittracker.py
+++ 8bittracker.py
@@ -1,4 +1,43 @@
 '''
 Main python file. Manages event loops.
 '''
-__author__ = 'Austin Scott'
+__author__ = 'Austin Scott and Samuel Volk'
+
+
+import pygame
+import gui
+
+# Init
+pygame.init()
+screen_surface = pygame.display.set_mode((gui.SCREEN_WIDTH, gui.SCREEN_HEIGHT))
+pygame.display.set_caption("8BitTracker")
+# Create character array to hold chars to be displayed and the colours (FG & BG)
+screen_chars = []
+for row in range(gui.SCREEN_HEIGHT):
+    screen_chars.append([])
+    for col in range(gui.SCREEN_WIDTH):
+        screen_chars[row].append([0,0,0])
+
+# Main Loop
+
+# set test data
+screen_chars[0][0] = [1, 14, 0]
+screen_chars[24][79] = [4, 12, 0]
+screen_chars[0][40] = [3, 9, 7]
+
+string = ""
+for i in xrange(36):
+    string += chr(i)
+gui.printl(screen_chars, string, 1, 0)
+gui.printl(screen_chars, "0 1 2 3 4 5 6 7 8 9", 3, 5, 5)
+gui.printl(screen_chars, "Hello, World!")
+
+
+# draw screen
+gui.draw_screen(screen_surface, screen_chars)
+pygame.display.flip()
+
+pygame.time.delay(10000)
+
+# Clean-up
+pygame.quit()
--- gui/__init__.py
+++ gui/__init__.py
@@ -12,7 +12,7 @@ FONT_HEIGHT = 12
 SCREEN_WIDTH = FONT_WIDTH * 80
 SCREEN_HEIGHT = FONT_HEIGHT * 25
 
-colour = [
+COLOR = [
     (0, 0, 0),
     (168, 0, 0),
     (0, 168, 0),
@@ -54,11 +54,11 @@ def draw_screen(screen, screen_array):
                 for x in xrange(FONT_WIDTH):
                     x2 = 7 - x
                     if (cp437[char[0]][y] & (1<<x)):
-                        pixelarray[x2][y] = colour[char[1]]
+                        pixelarray[x2][y] = COLOR[char[1]]
                     else:
-                        pixelarray[x2][y] = colour[char[2]]
+                        pixelarray[x2][y] = COLOR[char[2]]
             del pixelarray
-            screen_surface.blit(char_surface, dest)
+            screen.blit(char_surface, dest)
 
 def printl(screen_array, text, row=0, col=0, fg=7, bg=0):
     """Prints a line of text on the screen.
@@ -73,41 +73,3 @@ def printl(screen_array, text, row=0, col=0, fg=7, bg=0):
     """
     for i, char in enumerate(text):
         screen_array[row][col+i] = [ord(char), fg, bg]
-        #print ord(char)
-
-
-
-# Init
-pygame.init()
-screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("8BitTracker")
-# Create character array to hold chars to be displayed and the colours (FG & BG)
-screen_chars = []
-for row in range(SCREEN_HEIGHT):
-    screen_chars.append([])
-    for col in range(SCREEN_WIDTH):
-        screen_chars[row].append([0,0,0])
-
-# Main Loop
-
-# set test data
-screen_chars[0][0] = [1, 14, 0]
-screen_chars[24][79] = [4, 12, 0]
-screen_chars[0][40] = [3, 9, 7]
-
-string = ""
-for i in xrange(36):
-    string += chr(i)
-printl(screen_chars, string, 1, 0)
-printl(screen_chars, "0 1 2 3 4 5 6 7 8 9", 3, 5, 5)
-printl(screen_chars, "Hello, World!")
-
-
-# draw screen
-draw_screen(screen_surface, screen_chars)
-pygame.display.flip()
-
-pygame.time.delay(10000)
-
-# Clean-up
-pygame.quit()
